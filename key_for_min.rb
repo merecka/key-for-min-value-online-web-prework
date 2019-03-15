9a07e832ad7479054664224039344f70afa43eb5
Alex Merecka <merecka@gmail.com>
2019-03-15 23:43:08 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-# Strategy:  
-# 1) Convert hash into value array using .values method.
-# 2) Find length of values array using .length method.
-# 3) Iterate through values array using each an i & i+1.  Whichever value is least, save it to a variable.
-# 4) Use .key method to return the key for the value stored in the variable.
+# This is an example of short-circut evaluation error. This code will not work because the lowest_value == nil must be placed before the the_value < lowest_value due to short-circut evaluation.
+# https://stackoverflow.com/questions/32109701/comparison-of-fixnum-with-nil-failed-value-not-nil
+
+# def key_for_min_value(name_hash)
+# lowest_key = nil
+# lowest_value = nil
+# name_hash.each do |the_key, the_value|
+#   if (the_value < lowest_value) || (lowest_value == nil)
+#   lowest_value = the_value
+#   lowest_key = the_key
+#   end
+# end
+#  lowest_key
+# end
+
 
 def key_for_min_value(name_hash)
-  # if name_hash.length > 0
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |the_key, the_value|
-    if the_value < lowest_value || lowest_value == nil
+    if (lowest_value == nil) || (the_value < lowest_value)
     lowest_value = the_value
     lowest_key = the_key
     end
