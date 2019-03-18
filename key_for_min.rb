1a53d8ba982308f21e5b41c7f22d8d6ff6883d07
Alex Merecka <merecka@gmail.com>
2019-03-18 20:23:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -21,7 +21,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |the_key, the_value|
-    if (the_value < lowest_value) || (lowest_value == nil)
+    if (lowest_value == nil) || (the_value < lowest_value)
     lowest_value = the_value
     lowest_key = the_key
     end
